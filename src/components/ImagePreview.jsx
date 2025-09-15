f3ee0e6d01556efe4a4d3ee611519070cee6b103
ayushatree <ayushatre10@gmail.com>
2025-09-15 15:22:42 +0530

    fix: comment out download image functionality in ImagePreview component

--- src/components/ImagePreview.jsx
+++ src/components/ImagePreview.jsx
@@ -1,15 +1,15 @@
 import Loading from "./Loading";
 
 const ImagePreview = ({ uploaded, enhanced, loading }) => {
-  const downloadImage = () => {
-    if (!enhanced) return;
-    const link = document.createElement("a");
-    link.href = enhanced;
-    link.download = "enhanced.jpg";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
+  // const downloadImage = () => {
+  //   if (!enhanced) return;
+  //   const link = document.createElement("a");
+  //   link.href = enhanced;
+  //   link.download = "enhanced.jpg";
+  //   document.body.appendChild(link);
+  //   link.click();
+  //   document.body.removeChild(link);
+  // };
 
   return (
     <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-4xl">
@@ -49,14 +49,14 @@ const ImagePreview = ({ uploaded, enhanced, loading }) => {
             />
 
             {/* Transparent overlay button */}
-            <div className="absolute bottom-14 left-1/2  transform -translate-x-1/2">
+            {/* <div className="absolute bottom-14 left-1/2  transform -translate-x-1/2">
               <button
                 onClick={downloadImage}
                 className="px-6 py-2 rounded-xl backdrop-blur-md bg-white/30 text-white font-medium shadow-lg hover:bg-white/50 transition"
               >
                 ⬇ Download Image
               </button>
-            </div>
+            </div> */}
           </div>
         ) : (
           !loading && (
